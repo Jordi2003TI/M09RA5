204d746d730f7877ce12c20d76a5e42ec176312b
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-09-18 19:52:20 +0200

    avanze de clase

--- ROT13/SRC/Rot13.java
+++ ROT13/SRC/Rot13.java
@@ -3,38 +3,42 @@ import java.util.Scanner;
 public class Rot13 {
 
     // Variables globales
-    final String abd = "aàábcdeèéfghiíìjklmnoòópqrstùúvwxyz";
-    final String ABD = "AÀÁBCDEÈÉFGHIÍÌJKLMNOÒÓPQRSTÙÚVWXYZ";
+    static final String abd = "aàábcdeèéfghiíìjklmnoòópqrstùúvwxyz";
+    static final String ABD = "AÀÁBCDEÈÉFGHIÍÌJKLMNOÒÓPQRSTÙÚVWXYZ";
 
-    char[] abdArray = abd.toCharArray();
-    char[] ABDArray = ABD.toCharArray();
+    static char[] abdArray = abd.toCharArray();
+    static char[] ABDArray = ABD.toCharArray();
 
     public static void main(String[] args){
-    
-    System.out.println("Introduce un texto");
-    Scanner sc = new Scanner(System.in);
-    String cadena = sc.nextLine();
+    //String[] listaPrueba = ["hola", "Jaume", "Apruebame", "Jordi Masip Ibañez"]; // para poder hacer las pruebas del profe
 
-    String cifradoString = xifratRot13(cadena);
+    // Cifrar String
+    String cifradoString = xifratRot13("HolA que hace");
     System.out.println("Texto cifrado: " + cifradoString);
-    String descifradoString = desxifratRot13(cadena);
+
+    String descifradoString = desxifratRot13(cifradoString);
     }
 
     public static String xifratRot13(String cadena){
-        String cadenaCifrada = "";
+        // Pasar A cifrado
+        String cadenaCifradaMin = "";
         for(int i = 0; i < cadena.length(); i++){
             char c = cadena.charAt(i);
-            for(int j = 0; j < abdArray.length; j++){
+            for(int j = 0; j < abdArray.length; j++){ // Aprovechamos que son igual de largos
                 if(c == abdArray[j]){
-                    int posicion = (j + 13) % abdArray.length;
-                    cadenaCifrada += abdArray[posicion % abdArray.length];
+                    cadenaCifradaMin += abdArray[(j + 13 ) % abdArray.length];
+                }else if(c == ABDArray[j]){
+                    cadenaCifradaMin += ABDArray[(j + 13 ) % ABDArray.length];
+                }else{
+                    cadenaCifradaMin += c;
                 }
             }
         }
-        return cadenaCifrada;
+        return cadenaCifradaMin;
     }
 
     public static String desxifratRot13(String cadena){
+
         return "";
     }
 }
