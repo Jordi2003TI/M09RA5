5be4ce2e2591737a827fb13c1f997ae065e4fd12
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-09-19 13:14:23 +0200

    ejercicio acabado

--- ROT13/SRC/Rot13.java
+++ ROT13/SRC/Rot13.java
@@ -1,3 +1,11 @@
+/*
+ * M9 Jaume 
+ * DIA: 18/09/2025
+ * Jordi Masip Ibañez
+ * Es un ejercicio que esta pensado para poder cifrar cadenas de String conservando los espacios y caracteres que no sean letras. 
+ * Tenemos que poder descifrar tambien el mismo mensaje que hemos cifrado
+ */
+
 public class Rot13 {
 
     // Variables globales
@@ -8,13 +16,24 @@ public class Rot13 {
     static char[] ABDArray = ABD.toCharArray();
 
     public static void main(String[] args){
-    //String[] listaPrueba = ["hola", "Jaume", "Apruebame", "Jordi Masip Ibañez"]; // para poder hacer las pruebas del profe
+    String[] listaPrueba = {"hola", "Jaume", "Apruebame", "Jordi Masip Ibañez", "Tengo 22 años"}; // para poder hacer las pruebas del profe
 
+
+    for(String element : listaPrueba){
+        String cifrado = xifratRot13(element);
+        System.out.println("El texto cidrado es " + cifrado);
+        String desCifrado = desxifratRot13(cifrado);
+        System.out.println("El texto descifrado " + desCifrado);
+        System.out.println();
+    }
+    /* 
     // Cifrar String
     String cifradoString = xifratRot13("HolA que hace ??? ¡¡¡¡ ...");
     System.out.println("Texto cifrado: " + cifradoString);
 
     String descifradoString = desxifratRot13(cifradoString);
+    System.out.println("Texto descifrado " + descifradoString);
+    */
     }
 
     public static String xifratRot13(String cadena){
@@ -25,7 +44,7 @@ public class Rot13 {
             boolean encontrado = false;
             for(int j = 0; j < abdArray.length; j++){ // Aprovechamos que son igual de largos
                 if(c == abdArray[j]){
-                    cadenaCifradaMin += abdArray[(j + 13 ) % abdArray.length];
+                    cadenaCifradaMin += abdArray[(j + 13 ) % abdArray.length]; // Sirve para poder reinciar el indice de la array a traves de un calculo
                     encontrado = true;
                     break;
                 }else if(c == ABDArray[j]){
@@ -45,6 +64,27 @@ public class Rot13 {
 
     public static String desxifratRot13(String cadena){
 
-        return "";
+        String cadenaCifradaMin = "";
+        for(int i = 0; i < cadena.length(); i++){
+            char c = cadena.charAt(i);
+            boolean encontrado = false;
+            for(int j = 0; j < abdArray.length; j++){ 
+                if(c == abdArray[j]){
+                    cadenaCifradaMin += abdArray[(j - 13 + abdArray.length ) % abdArray.length]; // Ponemos que sume la array porque sino nos da un valor negativo y esto haria que nunca lo eocntrarse
+                    encontrado = true;
+                    break;
+                }else if(c == ABDArray[j]){
+                    cadenaCifradaMin += ABDArray[(j - 13 + ABDArray.length) % ABDArray.length];
+                    encontrado = true;
+                    break;
+                }
+            }
+            // Cuando hay un espcacio o un caracter difrente a una de las letras
+            if(!encontrado){
+                cadenaCifradaMin += c;
+            }
+            
+        }
+        return cadenaCifradaMin;
     }
 }
