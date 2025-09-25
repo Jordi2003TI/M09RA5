848d44704632a80d2c93206510d3b206d49d4bf8
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-09-25 19:57:35 +0200

    final classe

--- ROTX/SRC/RotX.java
+++ ROTX/SRC/RotX.java
@@ -20,10 +20,12 @@ public class RotX {
     String[] listaPrueba = {"hola", "Jaume?", "Apruebame", "Jordi Masip Ibañez", "Tengo 22 años"}; // para poder hacer las pruebas del profe
     String[] listaPruebaDescifrar = {"hola", "Láuoé?", "Ctúugècóg", "Òswhm Qexmú Mfedib", "Çìtmù 22 éèùz"};
     String[] listaXifrat = {"Òswhm Qexmú Mfedib"};
+
+
     System.out.println("Xifrat\n------");
     for(int i = 0; i < listaPrueba.length; i++){
         for(int y = i; y < listaPrueba.length; y++){
-            System.out.printf("(%d)%-30s \t => %-30s\n", i*2, listaPrueba[y], xifratRotX(listaPrueba[y], (i*2)));
+            System.out.printf("(%d)%-30s \t => %-30s\n", (i*2), listaPrueba[y], xifratRotX(listaPrueba[y], (i*2)));
             break;
         }
 
@@ -32,15 +34,19 @@ public class RotX {
     System.out.println("Desxifrat\n------");
     for(int i = 0; i < listaPruebaDescifrar.length; i++){
         for(int y = i; y < listaPruebaDescifrar.length; y++){
-            System.out.printf("(%d)%-30s \t => %-30s\n", i*2, listaPruebaDescifrar[y], desxifratRotX(listaPruebaDescifrar[y], (i*2)));
+            System.out.printf("(%d)%-30s \t => %-30s\n", (i*2), listaPruebaDescifrar[y], desxifratRotX(listaPruebaDescifrar[y], (i*2)));
             break;
         }
 
     }
     System.out.println();
-    for(int i = 0; i <= numeroMaxCifrado; i++){
-        for(int y = 0; y < listaXifrat.length; y++){
-            
+    for(int a = 0; a < listaXifrat.length; a++){
+        System.out.println("Missatge xifrat: " + listaXifrat[a]);
+        System.out.println("----------------");
+        for(int i = 0; i <= numeroMaxCifrado; i++){
+            for(int y = 0; y < listaXifrat.length; y++){
+                System.out.println("(" + i +")->" + forcaBrutaRotX());
+            }
         }
     }
     }
@@ -98,6 +104,27 @@ public class RotX {
     }
 
     public static void forcaBrutaRotX(String cadenaXifrada){
-        
+        String cadenaCifradaMin = "";
+        for(int i = 0; i < cadena.length(); i++){
+            char c = cadena.charAt(i);
+            boolean encontrado = false;
+            for(int j = 0; j < abdArray.length; j++){ 
+                if(c == abdArray[j]){
+                    cadenaCifradaMin += abdArray[(j - desplaçament + abdArray.length ) % abdArray.length]; // Ponemos que sume la array porque sino nos da un valor negativo y esto haria que nunca lo eocntrarse
+                    encontrado = true;
+                    break;
+                }else if(c == ABDArray[j]){
+                    cadenaCifradaMin += ABDArray[(j - desplaçament + ABDArray.length) % ABDArray.length];
+                    encontrado = true;
+                    break;
+                }
+            }
+            // Cuando hay un espcacio o un caracter difrente a una de las letras
+            if(!encontrado){
+                cadenaCifradaMin += c;
+            }
+            
+        }
+        System.err.println(cadenaCifradaMin);
     }
 }
