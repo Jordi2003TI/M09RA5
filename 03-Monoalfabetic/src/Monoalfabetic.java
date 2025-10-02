79802abf1ba6f48b2da0f0e08d03c04f7989c0ca
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-10-02 19:49:35 +0200

    03-Monoalfabetic final de classe

---
+++ 03-Monoalfabetic/src/Monoalfabetic.java
@@ -0,0 +1,70 @@
+/* 
+ * 
+ * 
+ * 
+*/
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.event.ListDataEvent;
+
+public class Monoalfabetic{
+    static final String ABD = "AÀÁBCDEÈÉFGHIÍÌJKLMNOÒÓPQRSTÙÚVWXYZÇÑ"; 
+    static final char[] ABDARRAY = ABD.toCharArray();
+    private static char[] ArraypPermutada;
+    public static void main(String[] args) {
+
+        ArraypPermutada = permutaAlfabet(ABDARRAY);
+        System.out.println(ABDARRAY);
+        System.out.println(ArraypPermutada);
+
+        String cifrado = xifraMonoAlfa("OsAmA  !!!! que tal como 123");
+        System.out.println(cifrado);
+
+    }
+    // Para permutar un alfabeto
+    public static char[] permutaAlfabet(char[] alfabeto){
+        List<Character> lista = new ArrayList<>();
+        for(char element : alfabeto){
+            lista.add(element);
+        }
+
+        // para poder mezclar la array
+        Collections.shuffle(lista);
+
+        char[] charPemutado = new char[alfabeto.length];
+        for(int i = 0; i < alfabeto.length; i++){
+            charPemutado[i] = lista.get(i);
+        }
+        return charPemutado;
+    }
+    // 
+    public static String xifraMonoAlfa(String cadena){
+        String cifrada = "";
+        for(int i = 0; i < cadena.length(); i++){
+            boolean EsLetra= false;
+            char c = cadena.charAt(i);
+            for(int y = 0; y < ABDARRAY.length; y++){
+                if(Character.toUpperCase(c) == ABDARRAY[y]){
+                    if(Character.isLowerCase(c)){
+                        cifrada += Character.toLowerCase(ArraypPermutada[y]);
+                        EsLetra = true;
+                    }else{
+                        cifrada += Character.toUpperCase(ArraypPermutada[y]);
+                        EsLetra = true;
+                    }          
+                }
+            }
+            if(!EsLetra){
+                cifrada += c;
+                continue;
+            }
+        }
+        return cifrada;
+    }
+
+    public static String desxifraMonoAlfa(String cadena){
+        
+        return null;
+    }
+}
